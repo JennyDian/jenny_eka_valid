042f5176f64eb1b7bc50f6bfd70a4743c6764864
JennyDian <vienstarr.j@gmail.com>
2026-06-13 22:03:51 +0700

    Update jennyekafiks.py

--- jennyekafiks.py
+++ jennyekafiks.py
@@ -201,7 +201,7 @@ with st.form("form_pemesanan", clear_on_submit=True):
         "Tanggal Lahir",
         min_value=date(1900, 1, 1),
         max_value=date(2008, 12, 31),
-        value=date(0000, 00, 0),
+        value=date(2000, 01, 1),
         format="YYYY/MM/DD"
     ) 
 
